e50d0f6be9969c90aa05e3680378a51967ea6409
Favour Egwele <favoregwele@gmail.com>
2025-02-28 16:43:59 -1200

    Following tut bcs I got stuck lol

--- rock.js
+++ rock.js
@@ -3,32 +3,24 @@ const stone = document.getElementById('rock');
 const  tissue = document.getElementById('paper');
 const cutter = document.getElementById('scissors');
 
-//MOVE TEXT
-const wordOne = document.getElementById('word1');
-
 //COUNTER
-let winCount = 0;
-let tieCount = 0;
-let loseCount = 0;
 
-const winText = document.querySelector('.win-count');
-const tieText = document.querySelector('.tie-count');
-const loseText = document.querySelector('.lose-count');
+const counter = JSON.parse(localStorage.getItem('counter'))
+
+console.log(counter);
 
 //RESET
 const reset = document.getElementById('reset');
 
-
-
 function computerMove() {
     let cMove = '';
-    let random = Math.random() * 3;
+    let random = Math.floor(Math.random() * 3);
 
-    if (random >= 0 && random < 1 / 3) {
+    if (random === 0) {
         cMove = 'Rock';
-    } else if (random >= 1 / 3 && random < 2 / 3) {
+    } else if (random === 1) {
         cMove = 'Paper';
-    } else if (random >= 2 / 3) {
+    } else if (random === 2) {
         cMove = 'Scissors';
     }
 
@@ -42,60 +34,42 @@ function playerMove(pMove) {
     if (pMove === 'Rock') {
         if (cMove == 'Paper') {
             result = 'You lose';
-            loseCount++;
-            loseText.innerText = loseCount;
-            console.log(cMove);
         } else if (cMove == 'Scissors') {
             result = 'You win';
-            winCount++;
-            winText.innerText = winCount;
-            console.log(cMove);
         } else {
             result = 'Its a tie';
-            tieCount++;
-            tieText.innerHTML = tieCount;
-            console.log(cMove);
         }
     } else if (pMove === 'Paper') {
         if (cMove == 'Paper') {
             result = 'Its a tie';
-            tieCount++;
-            tieText.innerText = tieCount;
-            console.log(cMove);
         } else if (cMove == 'Scissors') {
             result = 'You lose';
-            loseCount++;
-            loseText.innerText = loseCount;
-            console.log(cMove);
         } else {
             result = 'You win';
-            winCount++;
-            winText.innerText = winCount;
-            console.log(cMove);
         }
     } else {
         if (cMove == 'Paper') {
             result = 'You win';
-            winCount++;
-            winText.innerText = winCount;
-            console.log(cMove);
         } else if (cMove == 'Scissors') {
             result = 'Its a tie';
-            tieCount++;
-            tieText.innerText = tieCount;
-            console.log (cMove);
         } else {
             result = 'You lose';
-            loseCount++;
-            loseText.innerHTML = loseCount;
-            console.log(cMove);
         }
     }
 
-    wordOne.innerText = `You played ${pMove}. The computer played ${cMove}. ${result}.`;
-    setTimeout(() => { 
-        wordOne.innerText = '';
-    }, 3000);
+    if (result === 'You win') {
+        counter.win++;
+    } else if (result === 'You lose') {
+        counter.lose++;
+    } else {
+        counter.tie++;
+    }
+
+    localStorage.setItem('counter', JSON.stringify(counter));
+
+    console.log(cMove);
+    alert(`You played ${pMove}. The computer played ${cMove}. ${result}.
+Wins: ${counter.win}, Losses: ${counter.lose}, Tie: ${counter.tie}`);
 };
 
 stone.addEventListener('click', () => {    
@@ -113,11 +87,7 @@ cutter.addEventListener('click', () => {
 reset.onclick = resetGame;
 
 function resetGame() {
-    winCount = 0;
-    loseCount = 0;
-    tieCount = 0;
-
-    winText.innerText = winCount;
-    loseText.innerText = loseCount;
-    tieText.innerText = tieCount;
+    counter.win = 0;
+    counter.lose = 0;
+    counter.tie = 0; 
 };
